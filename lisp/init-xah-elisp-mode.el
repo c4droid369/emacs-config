dcc7090fa4b9673a1752a846a545e1b1e00c0c23
c4droid <c4droid@foxmail.com>
2024-11-26 02:36:34 +0800

    Cleaning rubbish code

--- lisp/init-xah-elisp-mode.el
+++
@@ -1,6 +0,0 @@
-;; -*- lexical-binding: t -*-
-(use-package xah-elisp-mode
-  :straight t
-  :disabled
-  :hook (emacs-lisp-mode . xah-elisp-mode))
-(provide 'init-xah-elisp-mode)
--- lisp/init-xah-fly-keys.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t -*-
-(use-package xah-fly-keys
-  :straight t
-  :disabled
-  :init
-  (xah-fly-keys-set-layout "qwerty")
-  (xah-fly-keys 1))
-(provide 'init-xah-fly-keys)
