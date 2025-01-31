42b3c0348f11d2d941afc8719c4c63012d46e7db
c4droid <c4droid@foxmail.com>
2025-01-31 17:58:00 +0800

    Add roam template and shortcut

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -20,6 +20,39 @@
   :straight t
   :custom
   (org-roam-directory (if is-win? "D:\\Documents\\Roam" "~/Note/Roam"))
+  (org-roam-completion-everywhere t)
+  (org-roam-capture-template '(("d" "default" plain
+                                "%?"
+                                :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+                                :unnarrowed t)))
   :config
-  (org-roam-db-autosync-mode))
+  (org-roam-db-autosync-mode)
+  (leader-def
+    "n" '(:ignore t)
+    "ni" 'org-roam-node-insert
+    "nf" 'org-roam-node-find
+    "nR" 'org-roam-node-random
+    "nc" 'org-roam-capture
+    "nr" 'org-roam-refile)
+  (localleader-def
+    :keymaps 'org-mode-map
+    :major-modes t
+    "n" '(:ignore t)
+    "no" 'org-roam-node-open
+    "nv" 'org-roam-node-visit))
+
+(use-package org-roam-ui
+  :straight t
+  :after org-roam
+  :diminish org-roam-ui-mode
+  :custom
+  (org-roam-ui-sync theme t)
+  (org-roam-ui-follow t)
+  (org-roam-ui-update-on-save t)
+  :config
+  (localleader-def
+    :keymaps 'org-mode-map
+    :major-modes t
+    "n" '(:ignore t)
+    "nu" 'org-roam-ui-open))
 (provide 'init-org)
